6cd417d5521baf0e6e01aa0c1af66c48f399e219
crackmanix <crackmanix@gmail.com>
2021-10-21 09:49:02 +0200

    init

---
+++ src/main/java/com/tlegrand/D2BuildMaker/D2BuildMakerApplication.java
@@ -0,0 +1,13 @@
+package com.tlegrand.D2BuildMaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2BuildMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D2BuildMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tlegrand/D2BuildMaker/D2BuildMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tlegrand.D2BuildMaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2BuildMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
